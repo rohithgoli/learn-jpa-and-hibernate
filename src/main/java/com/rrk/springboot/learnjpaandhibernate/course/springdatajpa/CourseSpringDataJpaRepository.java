24b7ada083e9f657bbcc48c1e27cbbd245141853
Rohith Goli <rohithgoli396@gmail.com>
2024-01-19 18:14:58 +0530

    feat: added custom methods with SpringDataJpa for querying Database

--- src/main/java/com/rrk/springboot/learnjpaandhibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
+++ src/main/java/com/rrk/springboot/learnjpaandhibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
@@ -1,9 +1,13 @@
 package com.rrk.springboot.learnjpaandhibernate.course.springdatajpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rrk.springboot.learnjpaandhibernate.course.Course;
 
 public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
 
+	List<Course> findByAuthor(String author);
+	List<Course> findByName(String name);
 }
